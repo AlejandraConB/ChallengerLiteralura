4540b8e4e025d91aa7056b1ea8487db8434bf1e0
Alejandra Contreras <154462336+AlejandraConB@users.noreply.github.com>
2025-01-22 00:33:22 -0300

    Update LiteraluraApplication.java

--- src/main/java/com/alura/literalura/LiteraluraApplication.java
+++ src/main/java/com/alura/literalura/LiteraluraApplication.java
@@ -2,12 +2,20 @@ package com.alura.literalura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import com.alura.literalura.presentacion.MenuPrincipal;
 
-@SpringBootApplication
-public class LiteraluraApplication {
+@SpringBootApplication  // Anotación de arranque de la aplicación Spring Boot
+public class LiterAluraApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LiteraluraApplication.class, args);
-	}
+		// Arrancar la aplicación Spring Boot
+		ApplicationContext context = SpringApplication.run(LiterAluraApplication.class, args);
 
+		// Obtener el bean de MenuPrincipal desde el contexto de Spring
+		MenuPrincipal menuPrincipal = context.getBean(MenuPrincipal.class);
+
+		// Llamar al método mostrarMenu para iniciar la aplicación.
+		menuPrincipal.mostrarMenu();
+	}
 }
